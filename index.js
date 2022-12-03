6d6c28fd2f413c41d7ce7e4e0fd5be90445c1e52
RockSTU <danielfariabomfim@gmail.com>
2022-12-03 04:41:39 -0300

    done

--- index.js
+++ index.js
@@ -23,7 +23,31 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
+//A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds (as type Number)
+app.get('/api/:date?', (req,res) => {
+    console.log('ESSE AQUI: ',new Date(parseInt(req.params.date)));
+        if ((new Date(req.params.date) == 'Invalid Date' && new Date(parseInt(req.params.date)) == 'Invalid Date') && req.params.date != undefined){
+            res.json({error: 'Invalid Date'})
+        }
+        else{
+            !req.params.date ? res.json({unix: Date.now(), utc: new Date().toGMTString()}) : 
+            (req.params.date.length == 13) ? res.json({unix: new Date(parseInt(req.params.date)).getTime(), utc: new Date(parseInt(req.params.date)).toGMTString()}):
+            res.json({unix: new Date(req.params.date).getTime(), utc: new Date(req.params.date).toGMTString()});
+        }
+        
+    
+//     if (req.params.date.length == 13){
+//     var timeUTC = new Date(parseInt(req.params.date));
+//     var UNIX = req.params.date.length
+//     }
+//     else {
+//         var timeUTC = new Date(req.params.date).toGMTString();
+//         var UNIX = new Date(req.params.date).getTime()
+//         console.log(UNIX)
+//     }
+//     res.json({unix: UNIX, utc: timeUTC})
+//     
+})
 
 
 // listen for requests :)
